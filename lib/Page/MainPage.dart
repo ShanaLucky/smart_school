1317b68855239895e080dc720b73ed61fcc541c4
HydroCarbon <hydrocarbonor@gmail.com>
2019-10-17 22:18:55 +0800

    Initial commit

---
+++ lib/Page/MainPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+/// App 登录页面
+/// 完成 App 的登录注册等功能
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState(); // 创建状态管理类
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'Page/MainPage.dart';
+
+/// 程序启动入口
+/// 实现程序的启动，加载启动页面
+
+void main() => runApp(MyApp()); // 运行程序
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HydroCarbon', // App Title
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // App 主题设置为蓝色
+      ),
+      home: LoginPage(), // 程序启动页面
+      debugShowCheckedModeBanner: false, // 关闭 Debug 标识
+    );
+  }
+}

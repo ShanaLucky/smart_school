93e89334ed57a8b5ec6d7f385e2e23f938ae6733
HydroCarbon <hydrocarbonor@gmail.com>
2019-10-18 15:04:35 +0800

    Add Function

--- lib/Page/MainPage.dart
+++ lib/Page/MainPage.dart
@@ -11,11 +11,48 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController _userNameController = TextEditingController();
+  TextEditingController _userPasswordController = TextEditingController();
+  GlobalKey _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HydroCarbon'),
+        title: Text('请登录'),
+        centerTitle: true,
+      ),
+      body: Form(
+        key: _formKey,
+        autovalidate: true,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              autofocus: true,
+              controller: _userNameController,
+              decoration: InputDecoration(
+                labelText: '邮箱',
+                hintText: '请输入邮箱',
+                icon: Icon(Icons.email),
+              ),
+              validator: (value) {
+                return value.trim().length > 0 ? null : '邮箱地址不正确';
+              },
+            ),
+            TextFormField(
+              controller: _userPasswordController,
+              decoration: InputDecoration(
+                labelText: '密码',
+                hintText: '请输入密码',
+                icon: Icon(Icons.lock),
+              ),
+              obscureText: true,
+              validator: (value) {
+                return value.trim().length > 5 ? null : '密码不能少于6位';
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
